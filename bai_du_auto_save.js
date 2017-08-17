743be3a3193de19a4aa070b9ef1a7670b95438e2
ckdkambh <brainbrain@126.com>
2017-08-18 00:14:37 +0800

    Update bai_du_auto_save.js

--- bai_du_auto_save.js
+++ bai_du_auto_save.js
@@ -30,22 +30,84 @@
                 window.location.href=result1;
             })
         };
+
+        var my_timer = function(length){
+            return new Promise(function (resolve, reject) {
+                setTimeout(resolve, length);
+            });
+        };
+
+        var my_timer_checker = function(fun){
+            console.log('my_timer_checker start');
+            return new Promise(function (resolve, reject) {
+                var loop = function(){
+                    my_timer(1000).then(function(){
+                        if (!fun()){
+                            loop();
+                        }else{
+                            console.log('my_timer_checker stop');
+                            resolve();
+                        }
+                    });
+                };
+                loop();
+            });
+        };
+
         getdata(4).then(function(result){
             console.log(result);
             if (result == '3' || result === '0'){
                 jumpHtml();
-            }else if (result == '1'){
-                getdata(1).then(function(result1){
-                    console.log(result1);
-                    $('#accessCode')[0].value = result1;
-                    $('span.text').click();
-                })
-            }else if (result == '2'){
-                setTimeout("if ($('span.zbyDdwb')[0] !== undefined) $('span.zbyDdwb')[0].click();",4000);
-                setTimeout("$('span.text')[0].click();",5000);
+            }
+
+            if (result == '1'){
+                if ($('#accessCode')[0] !== undefined){
+                    getdata(1).then(function(result1){
+                        console.log(result1);
+                        $('#accessCode')[0].value = result1;
+                        $('span.text').click();
+                    })
+                } else {
+                    getdata(1).then(function(result1){
+                        console.log(result1);
+                    })
+                    result = '2';
+                }
+            }
+
+            if (result == '2'){
                 getdata(2);
-                setTimeout("$('span.zbyDdwb')[0] !== undefined ? $('span.text')[18].click() : $('span.text')[7].click();",10000);
-                setTimeout("window.location.reload();",15000);
+                my_timer(3000).then(function(){
+                    if ($('span.zbyDdwb')[0] !== undefined){
+                        $('span.zbyDdwb')[0].click();
+                    }
+                });
+
+                my_timer(5000).then(function(){
+                    $('span.text')[0].click();
+                });
+
+                my_timer_checker(function(){
+                    return $('span.treeview-txt')[1] !== undefined;
+                }).then(function(){
+                    if ($('span.zbyDdwb')[0] !== undefined){
+                        $('span.text')[24].click();
+                    } else {
+                        $('span.text')[7].click();
+                    }
+                });
+
+                my_timer_checker(function(){
+                    if ($('span.tip-msg')[0] === undefined){
+                        return false;
+                    }else if ($('span.tip-msg').children('a')[0] === undefined){
+                        return false;
+                    }else{
+                        return $('span.tip-msg').children('a')[0].innerHTML === "点击查看";
+                    }
+                }).then(function(){
+                    window.location.reload();
+                });
             }
         });
     });
