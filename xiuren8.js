9ce4e87d6e51707fa4d21bfb9def9b3899edc939
ckdkambh <brainbrain@126.com>
2017-09-28 10:44:50 +0800

    Update xiuren8.js

--- xiuren8.js
+++ xiuren8.js
@@ -13,32 +13,55 @@
 'use strict';
 (function(){
     jQuery(document).ready(function() {
-        linkString = '';
-        linkCount = 0;
-        $("body").append('<div id="myDiv" style="display:none;"></div>');
-        var partList = $("a.product-title");
-        for (var i = 0; i < partList.length; i++){
-            var htmlobj=$.ajax({url:partList[i]["href"],async:false});
-            $("#myDiv").html(htmlobj.responseText);
-            var downLink;
-            try{
-                downLink = $($("#myDiv pre pre")[0])[0].innerHTML;
-            }catch(e){
-                try{
-                    downLink = $($("#myDiv pre")[0])[0].innerHTML;
-                }
-                catch(e){
-                    console.log('%o', partList[i].innerHTML);
-                    continue;
-                }
+       // $('#__QQCP_RIGHT_Div').remove();
+        //$('#__QQCP_LEFT_Div').remove();
+       // $('#__mhcm_r_b_4468').remove();
+
+        console.log('1234');
+        var c_n = $('body').children();
+        console.log('%o', c_n);
+        c_n.map((x)=>{
+            console.log('%o', c_n[x]);
+            console.log('checking %s', c_n[x].id);
+            if(c_n[x].id !== 'wrapper')
+            {
+                c_n[x].remove();
             }
-            console.log(downLink);
-            linkString += (downLink+'\n');
-            linkCount++;
-            $("#myDiv").html('');
-        }
-        GM_setClipboard(linkString);
-        console.log('已经复制'+linkCount+'条连接进入剪切板');
+        });
+
+        $('div.card-header').append('<button id="start_l">'+
+                                    '开始'+
+                                    '</button>');
+
+        $('#start_l').click(()=>{
+            console.log("#########");
+            linkString = '';
+            linkCount = 0;
+            $("body").append('<div id="myDiv" style="display:none;"></div>');
+            var partList = $("a.product-title");
+            for (var i = 0; i < partList.length; i++){
+                var htmlobj=$.ajax({url:partList[i]["href"],async:false});
+                $("#myDiv").html(htmlobj.responseText);
+                var downLink;
+                try{
+                    downLink = $($("#myDiv pre pre")[0])[0].innerHTML;
+                }catch(e){
+                    try{
+                        downLink = $($("#myDiv pre")[0])[0].innerHTML;
+                    }
+                    catch(e){
+                        console.log('%o', partList[i].innerHTML);
+                        continue;
+                    }
+                }
+                console.log(downLink);
+                linkString += (downLink+'\n');
+                linkCount++;
+                $("#myDiv").html('');
+            }
+            GM_setClipboard(linkString);
+            console.log('已经复制'+linkCount+'条连接进入剪切板');
+        });
     });
 })();
 
